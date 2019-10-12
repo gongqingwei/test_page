d05c728d4bcca2a748dae4197f90690fa5e1c334
gongqingwei <gongqingwei>
2019-10-13 00:14:29 +0800

    first commit

---
+++ js/js1.js
@@ -0,0 +1,19 @@
+/*function show_menu(){
+	//alert("click");
+	var menu1=document.getElementById("menu1");
+	if(menu1.style.display=="block"){
+		menu1.style.display="none";
+	}else{
+		menu1.style.display="block";
+	}	
+}*/
+function close_menu(){
+	//alert("close_menu");
+	var menu1=document.getElementById("menu1");
+	menu1.style.display="none";
+}
+function show_menu1(){
+	//alert("close_menu");
+	var menu1=document.getElementById("menu1");
+	menu1.style.display="block";
+}
